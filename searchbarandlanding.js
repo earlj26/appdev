3dfbb47e5b7d25fca3640cf48f3f0e3e20900746
alsonjohn33 <ajbangay100@gmail.com>
2024-12-23 16:27:12 +0800

    Update searchbarandlanding.js

--- searchbarandlanding.js
+++ searchbarandlanding.js
@@ -1,22 +1,13 @@
- document.addEventListener("click", function (e) {
-    const searchIcon = document.getElementById("searchIcon");
-    const searchBox = document.getElementById("searchBox");
+window.onload = function() {
 
-    if (!searchBox.contains(e.target) && !searchIcon.contains(e.target)) {
-      searchBox.classList.remove("show"); 
-    }
-  });
-
-  window.onload = function() {
-    document.body.classList.add('no-scroll');
-    setTimeout(function() {
-
-      document.getElementById('loading-screen').style.display = 'none';
-      document.getElementById('dashboard').style.display = 'block';
-      
-      document.body.classList.remove('no-scroll');
-    }, 3000);
-  }
-  
+  document.body.classList.add('no-scroll');
 
 
+  setTimeout(function() {
+    document.getElementById('loading-screen').style.display = 'none';
+    document.getElementById('dashboard').style.display = 'block';
+
+
+    document.body.classList.remove('no-scroll');
+  }, 3000);
+};
